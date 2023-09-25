3f5d7694fcb947e7c019aba986fb38807196802b
sartaj ansari <sartaj@eiysys.in>
2023-09-25 14:51:58 +0530

    express

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-console.log('Hello');
-console.log('jenkinsssss')
+// Import the Express.js framework
+const express = require('express');
 
-console.log("hello bhawesh")
-console.log("hhddsfsdhf")
+// Create an instance of the Express application
+const app = express();
+
+// Define a route for the root URL ("/")
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Define a route for "/about"
+app.get('/about', (req, res) => {
+  res.send('About Us');
+});
+
+// Define a route for "/contact"
+app.get('/contact', (req, res) => {
+  res.send('Contact Us');
+});
+
+// Start the server on port 3000
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
